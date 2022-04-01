72f369f07964b2fc7d6b7aad58a7cb175b7c62ee
Paulo DAlberti <phdalberti@gmail.com>
2022-04-01 15:23:57 +0900

    Display bolt iframe
    Steps 1 and 2 of https://github.com/nebulab/solidus_bolt/issues/7 displaying Bolt's iframe
    which requests credit card info to then tokenize. Further step will be using
    the token to authorize the payment.

--- app/assets/javascripts/spree/frontend/solidus_bolt.js
+++ app/assets/javascripts/spree/frontend/solidus_bolt.js
@@ -1,2 +1,34 @@
 // Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js"
+
+const displayBoltInput = (paymentField, boltContainer) => {
+  paymentField.mount(boltContainer);
+  const statusContainer = document.getElementById("payment-status-container");
+  statusContainer.style.display = "block";
+}
+
+const tokenize = async (paymentField) => {
+  await paymentField.tokenize()
+  .then((result) => {
+    if (result["token"]) {
+      // Submit a Payment Authorization POST Request
+    } else {
+      console.log(`error ${result["type"]}: ${result["message"]}`);
+    }
+  });
+}
+
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const boltContainer = document.getElementById("bolt-container");
+  const boltEmbedded = Bolt(boltContainer.dataset.publishableKey);
+
+  document.getElementById("bolt-card-button").addEventListener("click", () => {
+    const paymentField = boltEmbedded.create("payment_component");
+    displayBoltInput(paymentField, boltContainer);
+
+    document.getElementById("bolt-submit-button").addEventListener("click", () => {
+      tokenize(paymentField);
+    })
+  })
+})
