402af9bcfaa0ab4e794faa4ae6cdd3bb49725601
Paulo DAlberti <phdalberti@gmail.com>
2022-04-04 09:50:09 +0900

    Display account checkbox and listen for check

--- app/assets/javascripts/spree/frontend/solidus_bolt.js
+++ app/assets/javascripts/spree/frontend/solidus_bolt.js
@@ -1,10 +1,13 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js"
 
-const displayBoltInput = (paymentField, boltContainer) => {
+let createBoltAccount = false;
+
+const displayBoltInput = (paymentField, boltContainer, accountCheckbox) => {
   paymentField.mount(boltContainer);
   const statusContainer = document.getElementById("payment-status-container");
-  statusContainer.style.display = "block";
+  statusContainer.style.display = "";
+  accountCheckbox.mount("#account-checkbox")
 }
 
 const tokenize = async (paymentField) => {
@@ -14,10 +17,13 @@ const tokenize = async (paymentField) => {
 document.addEventListener("DOMContentLoaded", async function () {
   const boltContainer = document.getElementById("bolt-container");
   const boltEmbedded = Bolt(boltContainer.dataset.publishableKey);
+  const accountCheckbox = boltEmbedded.create("account_checkbox");
+
+  accountCheckbox.on("change", checked => createBoltAccount = checked);
 
   document.getElementById("bolt-card-button").addEventListener("click", () => {
     const paymentField = boltEmbedded.create("payment_component");
-    displayBoltInput(paymentField, boltContainer);
+    displayBoltInput(paymentField, boltContainer, accountCheckbox);
 
     document.getElementById("bolt-submit-button").addEventListener("click", () => {
       let result = tokenize(paymentField);
