eaefaefd3c6087f058e850ba38a8d48eebe394b7
piyushswain <piyush.swain3@gmail.com>
2022-04-19 17:40:59 +0530

    Add Edit page and Update action for Bolt Configuration
    This commit adds the edit page and related controller actions for the bolt configuration.
    
    The link to edit a bolt configuration is available at the end of the row for the listed bolt configuration on the index page.

--- app/controllers/spree/admin/bolts_controller.rb
+++ app/controllers/spree/admin/bolts_controller.rb
@@ -7,10 +7,36 @@ module Spree
 
       def show; end
 
+      def edit; end
+
+      def update
+        if @bolt_configuration.update(bolt_configuration_params)
+          flash[:success] = t('spree.admin.bolt.updated_successfully')
+          redirect_to admin_bolt_path
+        else
+          flash[:error] = @bolt_configuration.errors.full_messages.to_sentence
+          render :edit
+        end
+      end
+
       private
 
       def bolt_configuration
         @bolt_configuration = SolidusBolt::BoltConfiguration.fetch
+      end
+
+      def bolt_configuration_params
+        params
+          .require(:solidus_bolt_bolt_configuration)
+          .permit(
+            :bearer_token,
+            :environment_url,
+            :merchant_public_id,
+            :merchant_id,
+            :api_key,
+            :signing_secret,
+            :publishable_key
+          )
       end
     end
   end
--- spec/requests/spree/admin/bolt_spec.rb
+++ spec/requests/spree/admin/bolt_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 RSpec.describe "Spree::Admin::Bolts", type: :request do
   stub_authorization!
 
+  let(:bolt_configuration_params) {
+    {
+      bearer_token: SecureRandom.hex,
+      environment_url: SecureRandom.hex,
+      merchant_public_id: SecureRandom.hex,
+      merchant_id: SecureRandom.hex,
+      api_key: SecureRandom.hex,
+      signing_secret: SecureRandom.hex,
+      publishable_key: SecureRandom.hex
+    }
+  }
+
   describe "GET /show" do
     it 'returns a successful response' do
       get '/admin/bolt'
@@ -11,6 +23,49 @@ RSpec.describe "Spree::Admin::Bolts", type: :request do
 
     it 'creates a new SolidusBolt::BoltConfiguration record if no records are present' do
       expect { get '/admin/bolt' }.to change { SolidusBolt::BoltConfiguration.count }.by(1)
+    end
+  end
+
+  describe "GET /edit" do
+    let(:bolt_configuration) { create(:bolt_configuration) }
+
+    it 'returns a successful response' do
+      get '/admin/bolt/edit'
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "PUT /update" do
+    subject(:request) {
+      put '/admin/bolt', params: { solidus_bolt_bolt_configuration: bolt_configuration_params }
+    }
+
+    let(:bolt_configuration) { create(:bolt_configuration) }
+
+    it 'successfully redirects' do
+      request
+      expect(response.status).to eq 302
+    end
+
+    it 'redirects to index' do
+      request
+      expect(response).to redirect_to '/admin/bolt'
+    end
+
+    it 'successfully updates the bolt configuration' do
+      request
+
+      updated_attributes = SolidusBolt::BoltConfiguration.fetch.attributes.slice(
+        'bearer_token',
+        'environment_url',
+        'merchant_public_id',
+        'merchant_id',
+        'api_key',
+        'signing_secret',
+        'publishable_key'
+      )
+
+      expect(updated_attributes).to eq(bolt_configuration_params.deep_stringify_keys)
     end
   end
 end
