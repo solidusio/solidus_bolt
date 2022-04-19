2ec6eea0513dac11715959d060937e84f3d6f86b
piyushswain <piyush.swain3@gmail.com>
2022-04-19 13:39:26 +0530

    Add Transaction Capture API Object
    This commit adds the transaction capture api object.
    
    This object inherits from the SolidusBolt::Base Service Object and allows to call the /merchant/transactions/capture api for Bolt.
    
    This requires 3 arguments which are the `transaction_reference`, `amount` and `currency` which have to provided when the object for the class is initialized.
    
    Bolt API DOCS reference: https://docs.bolt.com/api-bolt/#operation/CaptureTransaction

---
+++ app/services/solidus_bolt/transaction/capture.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module SolidusBolt
+  module Transaction
+    class Capture < SolidusBolt::Base
+      attr_reader :transaction_reference, :amount, :currency
+
+      def initialize(transaction_reference:, amount:, currency:)
+        @transaction_reference = transaction_reference
+        @amount = amount
+        @currency = currency
+        super
+      end
+
+      def call
+        capture
+      end
+
+      private
+
+      def capture
+        options = build_options
+        HTTParty.post(
+          "#{api_base_url}/#{api_version}/merchant/transactions/capture",
+          options
+        )
+      end
+
+      def build_options
+        options = {
+          body: {
+            transaction_reference: transaction_reference,
+            amount: amount,
+            currency: currency
+          }.to_json,
+          headers: {
+            'X-Nonce' => generate_nonce,
+            'Content-Type' => 'application/json'
+          }
+        }
+
+        authenticate(options)
+        options
+      end
+    end
+  end
+end
