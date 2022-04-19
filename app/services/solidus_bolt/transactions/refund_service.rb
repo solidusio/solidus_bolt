9b50a5dc2c7d553370acdeb53733b05fc2c0042d
piyushswain <piyush.swain3@gmail.com>
2022-04-19 13:58:05 +0530

    Add Transaction Refund API Object
    This commit adds the transaction refund api object.
    
    This object inherits from the SolidusBolt::Base Service Object and allows to call the /merchant/transactions/credit api for Bolt.
    
    This requires 3 arguments which are the `transaction_reference`, `amount` and `currency` which have to provided when the object for the class is initialized.
    
    Bolt API DOCS reference: https://docs.bolt.com/api-bolt/#operation/refund

---
+++ app/services/solidus_bolt/transactions/refund_service.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module SolidusBolt
+  module Transactions
+    class RefundService < SolidusBolt::BaseService
+      attr_reader :transaction_reference, :amount, :currency
+
+      def initialize(transaction_reference:, amount:, currency:)
+        @transaction_reference = transaction_reference
+        @amount = amount
+        @currency = currency
+        super
+      end
+
+      def call
+        refund
+      end
+
+      private
+
+      def refund
+        options = build_options
+        HTTParty.post(
+          "#{api_base_url}/#{api_version}/merchant/transactions/credit",
+          options
+        )
+      end
+
+      def build_options
+        {
+          body: {
+            transaction_reference: transaction_reference,
+            amount: amount,
+            currency: currency
+          }.to_json,
+          headers: {
+            'X-Nonce' => generate_nonce,
+            'Content-Type' => 'application/json'
+          }
+        }.merge(authentication_header)
+      end
+    end
+  end
+end
