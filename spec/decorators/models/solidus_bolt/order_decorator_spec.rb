4739208c85a9ee50ecfc2790053951d89c85b5db
Paulo DAlberti <phdalberti@gmail.com>
2022-04-25 12:09:08 +0900

    Add order decorator with helper methods
    The methods added will serve to pass data in the right format to Bolt's API.

---
+++ spec/decorators/models/solidus_bolt/order_decorator_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe SolidusBolt::OrderDecorator do
+  let(:order) { create(:order_with_line_items) }
+
+  describe '#bolt_cart' do
+    it 'returns a hash with line items and price' do
+      result = {
+        total_amount: (order.total * 100).to_i,
+        order_reference: order.number,
+        currency: 'USD',
+        items: [{
+          sku: order.line_items.first.sku,
+          name: order.line_items.first.name,
+          unit_price: 1000,
+          quantity: 1
+        }]
+      }
+      expect(order.bolt_cart).to eq(result)
+    end
+  end
+
+  describe '#bolt_user_identifier' do
+    it 'returns a hash with email and phone' do
+      result = { email: order.email, phone: '555-555-0199' }
+      expect(order.bolt_user_identifier).to eq(result)
+    end
+  end
+
+  describe '#bolt_user_identity' do
+    it 'returns a hash with name' do
+      result = { 'first_name': 'John', 'last_name': 'Doe' }
+      expect(order.bolt_user_identity).to eq(result)
+    end
+  end
+end
