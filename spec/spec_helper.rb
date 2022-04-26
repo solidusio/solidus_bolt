5ee241f83e6faee8d954d19c9355c3b715277731
piyushswain <piyush.swain3@gmail.com>
2022-04-26 10:48:04 +0530

    Add and Configure VCR
    This commit adds the `vcr` and `webmock` gems as development dependencies for the project.
    
    The configuration for `vcr` has also been added to the spec_helper.rb file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ ENV['RAILS_ENV'] = 'test'
 # Run Coverage report
 require 'solidus_dev_support/rspec/coverage'
 require 'pry'
+require 'vcr'
+require 'webmock/rspec'
 
 # Create the dummy app if it's still missing.
 dummy_env = "#{__dir__}/dummy/config/environment.rb"
@@ -29,4 +31,25 @@ RSpec.configure do |config|
   if Spree.solidus_gem_version < Gem::Version.new('2.11')
     config.extend Spree::TestingSupport::AuthorizationHelpers::Request, type: :system
   end
+end
+
+# Configure VCR
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+
+  config.filter_sensitive_data('<API_KEY>') do |interaction|
+    interaction.request.headers['X-API-KEY']&.first
+  end
+
+  config.filter_sensitive_data('<PUBLISHABLE_KEY>') do |interaction|
+    interaction.request.headers['X-Publishable-Key']&.first
+  end
+
+  # Let's you set default VCR record mode with VCR_RECORDE_MODE=all for re-recording
+  # episodes. :once is VCR default
+  record_mode = ENV.fetch('VCR_RECORD_MODE', :once).to_sym
+  config.default_cassette_options = { record: record_mode }
 end
