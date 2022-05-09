b0cccffe0b619404f0474ddb923b8794afc9b82d
Paulo DAlberti <phdalberti@gmail.com>
2022-05-09 19:15:17 +0900

    Add iframe preferences to SolidusBolt::PaymentMethod
    publishable_key and base_url are needed to display the Bolt iframe. This commit
    adds both of them making sure that base_url is dependent on the selected environment
    instead of having to manually write it.

--- app/models/solidus_bolt/payment_method.rb
+++ app/models/solidus_bolt/payment_method.rb
@@ -4,6 +4,10 @@ module SolidusBolt
   class PaymentMethod < SolidusSupport.payment_method_parent_class
     preference :bolt_api_key, :string
     preference :bolt_signing_secret, :string
+    preference :bolt_publishable_key, :string
+    preference :bolt_environment, :string
+
+    validates :preferred_bolt_environment, inclusion: { in: %w[production sandbox staging] }, allow_blank: true
 
     def gateway_class
       ::SolidusBolt::Gateway
@@ -15,6 +19,12 @@ module SolidusBolt
 
     def partial_name
       'bolt'
+    end
+
+    def preferred_bolt_base_url
+      return nil if preferred_bolt_environment.nil?
+
+      preferred_bolt_environment == 'production' ? 'https://connect.bolt.com' : 'https://connect-sandbox.bolt.com'
     end
   end
 end
--- spec/models/solidus_bolt/payment_method_spec.rb
+++ spec/models/solidus_bolt/payment_method_spec.rb
@@ -18,4 +18,39 @@ RSpec.describe SolidusBolt::PaymentMethod, type: :model do
       expect(described_class.new.partial_name).to eq 'bolt'
     end
   end
+
+  describe '#preferred_bolt_base_url' do
+    context 'when environment is nil' do
+      let(:payment_method) { create(:bolt_payment_method, preferences: { bolt_environment: nil }) }
+
+      it { expect(payment_method.preferred_bolt_base_url).to eq(nil) }
+    end
+
+    context 'when environment is production' do
+      let(:payment_method) { create(:bolt_payment_method, preferences: { bolt_environment: 'production' }) }
+
+      it { expect(payment_method.preferred_bolt_base_url).to eq('https://connect.bolt.com') }
+    end
+
+    context 'when environment is staging' do
+      let(:payment_method) { create(:bolt_payment_method, preferences: { bolt_environment: 'staging' }) }
+
+      it { expect(payment_method.preferred_bolt_base_url).to eq('https://connect-sandbox.bolt.com') }
+    end
+
+    context 'when environment is sandbox' do
+      let(:payment_method) { create(:bolt_payment_method) }
+
+      it { expect(payment_method.preferred_bolt_base_url).to eq('https://connect-sandbox.bolt.com') }
+    end
+  end
+
+  describe 'validations' do
+    let(:payment_method) { build(:bolt_payment_method, preferences: { bolt_environment: 'localhost' }) }
+
+    it 'raises an error when creating a SolidusBolt::PaymentMethod with wrong environment' do
+      payment_method.valid?
+      expect(payment_method.errors.messages).to eq({ preferred_bolt_environment: ['is not included in the list'] })
+    end
+  end
 end
