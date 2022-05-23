e0654327792865df2809bea34b7e524c46583572
Paulo DAlberti <phdalberti@gmail.com>
2022-05-23 10:18:36 +0900

    Implement the payment source syncing
    The payment source syncing will happen as part of the checkout flow on
    then /checkout/payment page.

--- spec/requests/spree/checkout_controller_spec.rb
+++ spec/requests/spree/checkout_controller_spec.rb
@@ -3,19 +3,21 @@ require 'spec_helper'
 RSpec.describe "Spree::CheckoutController", type: :request do
   stub_authorization!
 
+  let(:user) { order.user }
+
+  before do
+    # rubocop:disable RSpec/AnyInstance
+    allow_any_instance_of(Spree::CheckoutController).to receive(:current_order).and_return(order)
+    allow_any_instance_of(SolidusBolt::BoltConfiguration).to(
+      receive(:embed_js).and_return('https://connect-sandbox.bolt.com/embed.js')
+    )
+    # rubocop:enable RSpec/AnyInstance
+  end
+
   describe "GET /checkout/address" do
-    let(:user) { order.user }
     let(:order) { Spree::TestingSupport::OrderWalkthrough.up_to(:address) }
 
-    before do
-      allow(SolidusBolt::Users::SyncAddressesService).to receive(:call)
-      # rubocop:disable RSpec/AnyInstance
-      allow_any_instance_of(Spree::CheckoutController).to receive(:current_order).and_return(order)
-      allow_any_instance_of(SolidusBolt::BoltConfiguration).to(
-        receive(:embed_js).and_return('https://connect-sandbox.bolt.com/embed.js')
-      )
-      # rubocop:enable RSpec/AnyInstance
-    end
+    before { allow(SolidusBolt::Users::SyncAddressesService).to receive(:call) }
 
     it 'returns a successful response' do
       get '/checkout/address'
@@ -27,6 +29,24 @@ RSpec.describe "Spree::CheckoutController", type: :request do
       get '/checkout/address'
 
       expect(SolidusBolt::Users::SyncAddressesService).to have_received(:call)
+    end
+  end
+
+  describe "GET /checkout/payment" do
+    let(:order) { Spree::TestingSupport::OrderWalkthrough.up_to(:payment) }
+
+    before { allow(SolidusBolt::Users::SyncPaymentSourcesService).to receive(:call) }
+
+    it 'returns a successful response' do
+      get '/checkout/payment'
+
+      expect(response.status).to eq 200
+    end
+
+    it 'calls the service to sync payment sources' do
+      get '/checkout/payment'
+
+      expect(SolidusBolt::Users::SyncPaymentSourcesService).to have_received(:call)
     end
   end
 end
