8fce554c45071b286600328bc24c3838d01531cb
Paulo DAlberti <phdalberti@gmail.com>
2022-05-23 15:49:26 +0900

    Make SolidusBolt::PaymentSource reusable
    Reusability should depend on the presence of the card_id on payment
    source. If the PS is reusable, let's display its data at the payment
    stage of the checkout process.

--- app/models/solidus_bolt/payment_source.rb
+++ app/models/solidus_bolt/payment_source.rb
@@ -5,5 +5,9 @@ require_dependency 'solidus_bolt'
 module SolidusBolt
   class PaymentSource < SolidusSupport.payment_source_parent_class
     validates :payment_method_id, presence: true
+
+    def reusable?
+      card_id.present?
+    end
   end
 end
