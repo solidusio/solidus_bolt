0f8c7f4120a1030d4d9c248f1d896fa874079b19
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-07-21 11:12:30 +0200

    Set default BOLT_ENVIRONMENT
    When BOLT_ENVIRONMENT is not specified the setup fails with:
    
    ```
    PG::NotNullViolation: ERROR:  null value in column "environment" violates not-null constraint
    ```
    
    This commit provides a default value for it

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 solidus_bolt_configuration = SolidusBolt::BoltConfiguration.fetch
 
-solidus_bolt_configuration.environment = ENV['BOLT_ENVIRONMENT']
+solidus_bolt_configuration.environment = ENV.fetch('BOLT_ENVIRONMENT', 'sandbox')
 solidus_bolt_configuration.merchant_public_id = ENV['BOLT_MERCHANT_PUBLIC_ID']
 solidus_bolt_configuration.division_public_id = ENV['BOLT_DIVISION_PUBLIC_ID']
 solidus_bolt_configuration.api_key = ENV['BOLT_API_KEY']
