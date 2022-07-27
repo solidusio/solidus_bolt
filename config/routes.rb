5f88802e93eb42e550efdee291ac803739b75ff6
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2022-07-27 19:06:19 +0200

    Add bolt_logout route
    Add a GET route for the logout path.
    This route is required because at the end of the logout process, Bolt
    redirects the user to this page.

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Spree::Core::Engine.routes.draw do
 
   post '/webhooks/bolt', to: '/solidus_bolt/webhooks#update'
   post '/api/accounts/bolt', to: '/solidus_bolt/accounts#create'
+
+  devise_scope :spree_user do
+    get '/bolt_logout', to: '/spree/user_sessions#destroy', as: 'bolt_logout'
+  end
 end
