71a043ea475315a7e6141422e86a1d1c9b2e4ce1
Marc Busqué <marc@lamarciana.com>
2022-08-17 12:37:45 +0200

    Configure static sources within to_prepare blocks
    This is needed in Rails 7 [1] and latest Solidus was adapted to support
    it. See https://github.com/solidusio/solidus/pull/4449
    
    [1] - https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoload-on-boot-and-on-each-reload

--- lib/solidus_bolt/engine.rb
+++ lib/solidus_bolt/engine.rb
@@ -13,16 +13,18 @@ module SolidusBolt
 
     initializer "solidus_bolt.add_static_preference", after: "spree.register.payment_methods" do |app|
       app.config.spree.payment_methods << 'SolidusBolt::PaymentMethod'
-      Spree::Config.static_model_preferences.add(
-        SolidusBolt::PaymentMethod,
-        'bolt_credentials',
-        bolt_credentials_hash
-      )
-      Spree::Config.static_model_preferences.add(
-        SolidusBolt::PaymentMethod,
-        'bolt_config_credentials',
-        bolt_config_credentials_hash
-      )
+      app.config.to_prepare do
+        Spree::Config.static_model_preferences.add(
+          SolidusBolt::PaymentMethod,
+          'bolt_credentials',
+          ::SolidusBolt::Engine.bolt_credentials_hash
+        )
+        Spree::Config.static_model_preferences.add(
+          SolidusBolt::PaymentMethod,
+          'bolt_config_credentials',
+          ::SolidusBolt::Engine.bolt_config_credentials_hash
+        )
+      end
 
       Spree::PermittedAttributes.source_attributes.concat(%i[
         card_token card_last4 card_bin card_number card_expiration card_postal_code create_bolt_account
@@ -34,29 +36,29 @@ module SolidusBolt
       g.test_framework :rspec
     end
 
-    private
-
-    def bolt_credentials_hash
-      {
-        bolt_api_key: ENV['BOLT_API_KEY'],
-        bolt_signing_secret: ENV['BOLT_SIGNING_SECRET'],
-        bolt_publishable_key: ENV['BOLT_PUBLISHABLE_KEY'],
-      }
-    end
-
-    def bolt_config_credentials_hash
-      begin
-        bolt_config = SolidusBolt::BoltConfiguration.fetch
-      rescue ActiveRecord::StatementInvalid
-        bolt_config = nil
-      ensure
-        bolt_config_hash = {
-          bolt_api_key: bolt_config&.api_key,
-          bolt_signing_secret: bolt_config&.signing_secret,
-          bolt_publishable_key: bolt_config&.publishable_key,
+    class << self
+      def bolt_credentials_hash
+        {
+          bolt_api_key: ENV['BOLT_API_KEY'],
+          bolt_signing_secret: ENV['BOLT_SIGNING_SECRET'],
+          bolt_publishable_key: ENV['BOLT_PUBLISHABLE_KEY'],
         }
       end
-      bolt_config_hash
+
+      def bolt_config_credentials_hash
+        begin
+          bolt_config = SolidusBolt::BoltConfiguration.fetch
+        rescue ActiveRecord::StatementInvalid
+          bolt_config = nil
+        ensure
+          bolt_config_hash = {
+            bolt_api_key: bolt_config&.api_key,
+            bolt_signing_secret: bolt_config&.signing_secret,
+            bolt_publishable_key: bolt_config&.publishable_key,
+          }
+        end
+        bolt_config_hash
+      end
     end
   end
 end
